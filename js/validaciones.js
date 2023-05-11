af272563e42b1e88444a018924d41e9b1c9e9914
archersmack <gndaro_13@hotmail.com>
2023-05-10 19:51:05 -0500

    Se meojran las validaciones de los formularios y se realiza codigo con JS para autenticación de usuario y contraseña en el login

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,6 +1,5 @@
-
-// FUNCIONES
-function validar(eventoBlur){
+// =============== FUNCIONES =============== 
+function validarInput(eventoBlur){
     const tipoDeInputValidacion = eventoBlur.dataset.tipoinput;
 
     if (eventoBlur.validity.valid) {
@@ -16,35 +15,47 @@ function validar(eventoBlur){
                 eventoBlur.parentElement.querySelector(".input__messageError").innerHTML = "El campo  EMAIL no es válido o está incompleto. <br> El formato es: email@dominio.com";
                 break;
             case "contraseña":
-                eventoBlur.parentElement.querySelector(".input__messageError").innerHTML = "El campo  CONTRASEÑA no es válido o está incompleto. <br> Debe tener entre 8 y 12 caracteres, debe tener mínimo una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.";
+                eventoBlur.parentElement.querySelector(".input__messageError").innerHTML = "El campo  CONTRASEÑA no es válido o está incompleto. <br> Debe tener entre 8 y 15 caracteres, debe tener mínimo una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales.";
                 break;
             case "nombre":
                 eventoBlur.parentElement.querySelector(".input__messageError").innerHTML = "El campo  NOMBRE no es válido o está incompleto. <br> Debe contener mínimo 3 caracteres.";
                 break;
             case "mensaje":
-                eventoBlur.parentElement.querySelector(".input__messageError").innerHTML = "El campo  MENSAJE no es válido o está incompleto. <br> Debe contener mínimo 1 caracter.";
+                eventoBlur.parentElement.querySelector(".input__messageError").innerHTML = "El campo  MENSAJE no es válido o está incompleto. <br> Debe contener mínimo 1 caracter y máximo 120.";
                 break;
-        }
-    }
-}
+        };
+    };
+};
 
 
-//  CUERPO ALGORITMO
+//  =============== CUERPO ALGORITMO =============== 
 const inputs = document.querySelectorAll("[data-tipoinput]");   // con querySelectorAll se selecciona todos los inputs data-tipoInput
 console.log(inputs);
 
+//   Validación de inputs
 inputs.forEach( (cadaInput) => {                                // Ahora se reccorre cada input y se escucha el evento BLUR de cada uno
     cadaInput.addEventListener("blur", (eventoBlur) => {
-        validar(eventoBlur.target);                             // Se envia el evento como parámetro a la función validar
+        validarInput(eventoBlur.target);                        // Se envia el evento como parámetro a la función validar
         console.log(cadaInput);                                 // En la consola se muestra la lista con la cantidad de elementos Input encontrados
-    })
-})
-/*
-emailCliente[0].addEventListener("blur", (eventoBlur) => {          //Se busca el valor cero porque lo considera una listA (nodeList)
-    validar(eventoBlur.target);
-})
+    });
+});
 
-const passwordCliente = document.querySelectorAll("[data-tipo]");  //passwordcliente se crea como un NodeList
-passwordCliente[0].addEventListener("blur", (eventoBlur) => {          //Se busca el valor cero porque lo considera una listA (nodeList)
-    validar(eventoBlur.target);
-})*/
+// =============== Autenticación de usuario ===============
+
+const botonValidUser = document.querySelector("[data-botonValidarUsuario]");
+
+botonValidUser.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    const usuarioAsignado = "adrianapao1118@hotmail.com";
+    const passwordAsignada = "Laveterinaria1";
+    const user = document.querySelector("#user").value;
+    const password = document.querySelector("#password").value;
+    
+    if (usuarioAsignado == user && passwordAsignada == password){
+        window.location.href = "../links/03_menuAdministrador.html";
+    }else{
+        alert("EL usuario o contraseña está errado.");
+        //window.location.href = "../links/03_menuAdministrador.html";
+    }
+    
+});
